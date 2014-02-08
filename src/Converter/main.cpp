d38103f95cb88c9f2a3564bf00dae7de75f45574
Jesse_V <jvictors@jessevictors.com>
2014-02-08 01:07:15 -0700

    fixed box compression

--- src/Converter/main.cpp
+++ src/Converter/main.cpp
@@ -6,26 +6,37 @@
 #include <iostream>
 
 const std::size_t SIZE = 1024;
-const std::size_t MAX_BOX_SIZE = 128;
+const std::size_t MAX_BOX_SIZE = 256;
 
 int main(int argc, char** argv)
 {
     Matrix2D matrix = readMatrix(std::string("multibrot,d=2.dat"));
 
-    std::cout << "Writing... ";
-    writeMatrix(matrix, std::string("image_pre.ppm"));
+    std::cout << "Writing pre image... ";
+    writeMatrix(matrix, std::string("image_pre.ppm"), 1);
     std::cout << "done." << std::endl;
 
-    std::cout << "Compressing at level ";
-    for (std::size_t level = 2; level <= MAX_BOX_SIZE; level *= 2)
+    std::cout << "Compressing. Looking for boxes of size ";
+    short max = 0;
+    for (std::size_t lookFor = 1; lookFor < MAX_BOX_SIZE; lookFor *= 2)
     {
-        std::cout << level << " ";
-        compress(matrix, level);
+        std::cout << lookFor << " ";
+        auto val = compress(matrix, lookFor);
+        if (val > max)
+            max = val;
     }
     std::cout << " done." << std::endl;
 
-    std::cout << "Writing... ";
-    writeMatrix(matrix, std::string("image_post.ppm"));
+    //for (std::size_t x = 0; x <= SIZE - 32; x += 32)
+    //    for (std::size_t y = 0; y < SIZE; y++)
+    //        matrix[x][y] = 0;
+
+    std::cout << "Writing post image... ";
+    writeMatrix(matrix, std::string("image_post.ppm"), max);
+    std::cout << "done." << std::endl;
+
+    std::cout << "Writing geometry... ";
+    writeGeometry(matrix, std::string("geometry.dat"), max);
     std::cout << "done." << std::endl;
 
     std::cout << "Program complete." << std::endl;
@@ -70,39 +81,50 @@ Matrix2D readMatrix(std::string filename)
 
 
 
-void compress(Matrix2D& matrix, std::size_t level)
+short compress(Matrix2D& matrix, std::size_t lookingFor)
 {
-    for (std::size_t x = 0; x < SIZE - level; x += level)
+    size_t newSize = lookingFor * 2;
+    bool found = false;
+
+    for (std::size_t x = 0; x <= SIZE - newSize; x += newSize)
     {
-        for (std::size_t y = 0; y < SIZE - level; y += level)
+        for (std::size_t y = 0; y <= SIZE - newSize; y += newSize)
         {
-            if (matrix[x][y] == matrix[x + level][y] &&
-                matrix[x][y] == matrix[x + level][y + level] &&
-                matrix[x][y] == matrix[x][y + level]
-            )
-                for (std::size_t j = 0; j < level; j++)
-                    for (std::size_t k = 0; k < level; k++)
-                        matrix[x + j][y + k] = matrix[x][y] * 2;
+            if (matrix[x][y] == lookingFor)
+            {
+                if (lookingFor == matrix[x + lookingFor][y] &&
+                    lookingFor == matrix[x + lookingFor][y + lookingFor] &&
+                    lookingFor == matrix[x][y + lookingFor]
+                )
+                    for (std::size_t j = 0; j < newSize; j++)
+                        for (std::size_t k = 0; k < newSize; k++)
+                            matrix[x + j][y + k] = newSize;
+
+                found = true;
+            }
         }
     }
+
+    return found ? lookingFor : 0;
 }
 
 
 
-void writeMatrix(Matrix2D& matrix, std::string filename)
+void writeMatrix(Matrix2D& matrix, std::string filename, short max)
 {
     std::ofstream fout;
     fout.open(filename, std::ofstream::out);
 
-    fout << "P2 " << SIZE << " " << SIZE << " 255" << std::endl;
+    fout << "P3 " << SIZE << " " << SIZE << " 255" << std::endl;
     for (const auto &row : matrix)
     {
         for (const auto &cell : row)
         {
+            int gray = (int)(cell / (float)max * 255);
             if (cell == 0)
-                fout << "255 ";
+                fout << "0 0 255 ";
             else
-                fout << (int)(cell / (float)MAX_BOX_SIZE * 255) << " ";
+                fout << gray << " " << gray << " " << gray << " ";
         }
 
         fout << std::endl;
--- src/Converter/main.hpp
+++ src/Converter/main.hpp
@@ -8,7 +8,7 @@
 typedef std::vector<std::vector<short>> Matrix2D;
 
 Matrix2D readMatrix(std::string filename);
-void compress(Matrix2D& matrix, std::size_t level);
-void writeMatrix(Matrix2D& matrix, std::string filename);
+short compress(Matrix2D& matrix, std::size_t level);
+void writeMatrix(Matrix2D& matrix, std::string filename, short max);
 
 #endif
